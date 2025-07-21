48856718e0e1ff12307c1f2314230beb8c3b3f35
Sujan Aryal <sujanaryal@Sujans-MacBook-Pro.local>
2025-07-21 19:47:58 +1000

    define class, objects and access them

---
+++ defclass.py
@@ -0,0 +1,60 @@
+#here we  learn to define the class
+
+
+
+
+#syntax
+# class ClassName:
+    #provide class description
+    
+'''class Bike: # Bike is the name of the class
+    name: "abc" #variables with some values
+    gear = 0'''
+    #the variables created inside the class are called attributes.
+    
+    
+    
+#objects
+
+#so object is called an instance of class
+
+#example
+
+#syntax of object
+#objectName = ClassName()
+
+
+class Bike: # Bike is the name of the class
+    name = "Hey" #variables with some values
+    gear = 10
+    #the variables created inside the class are called attributes.
+
+#object of the class
+bike1 = Bike() #bike1 is the obj of the class called Bike()
+#after creating this object of the class, access the class attributes.
+
+
+#Example
+#Accessing the class Attributes Using Objects
+
+#trying to access name propetry (attributes)
+bike1.name = "Mountain Bike"
+#accessing the 2nd attribute of the class
+bike1.gear = 5
+
+print(f'Name: {bike1.name}, Gear: {bike1.gear}')
+
+
+
+#define a class
+#create attributes
+#create object of the class (two objects)
+#access the attributes and even modify it (. notation)
+
+
+    
+    
+
+  
+
+  
